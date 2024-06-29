1dcec7763af8d17c2e0d39923cfa1ceeebfa5a53
Yuhooong <2022011198@bistu.edu.cn>
2024-06-29 11:10:42 +0800

    地图可视化
    展示各个城市的二手车数量

--- data_analysis(3)/小学期_数据分析.py
+++ data_analysis(3)/小学期_数据分析.py
@@ -15,13 +15,12 @@ print(data)
 # 创建地图对象，并设置城市和销量数据
 m = (
     Map()
-    .add(二手车数量, [list(z) for z in zip(data['city'], data['car_counts'])], china-cities, label_opts=opts.LabelOpts(is_show=False),)
+    .add("二手车数量", [list(z) for z in zip(data['city'], data['car_counts'])], "china-cities", label_opts=opts.LabelOpts(is_show=False),)
     .set_global_opts(
-        title_opts=opts.TitleOpts(title=各城市二手车数量地图),
+        title_opts=opts.TitleOpts(title="各城市二手车数量地图"),
         # visualmap_opts=opts.VisualMapOpts(is_piecewise=True),  # 分段颜色显示
     )
 )
 
 # 渲染地图到HTML文件
-m.render(car_map.html)
-
+m.render("car_map.html")
