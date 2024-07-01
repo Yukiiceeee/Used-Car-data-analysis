1f0765028ebd19219fd76d00b70001d24d338dab
missbyebye <2022011197@bistu.edu.cn>
2024-07-01 10:20:48 +0800

    Update 数据预处理.py

--- 数据预处理.py
+++ 数据预处理.py
@@ -16,7 +16,7 @@ print('Train data shape:',Train_data.shape)
 print('TestA data shape:',Test_data.shape)
 # 获取数据前5行
 print(Train_data.head())
-
+# info()获取基本信息
 print(Train_data.info())
 # 通过describe()来熟悉数据的相关统计量
 print(Train_data.describe(include='all'))
@@ -30,14 +30,24 @@ numeric_features = ['power', 'kilometer', 'v_0', 'v_1', 'v_2', 'v_3', 'v_4', 'v_
 # # 类型特征
 categorical_features = ['name', 'model', 'brand', 'bodyType', 'fuelType', 'gearbox', 'notRepairedDamage', 'regionCode',]
 
-# 特征nunique分布
+# 特征nunique分布，对于类别特征，查看value_counts统计值，观察是否有异常值
 for cat_fea in categorical_features:
     # print(cat_fea + "的特征分布如下：")
     print("{}特征有个{}不同的值".format(cat_fea, Train_data[cat_fea].nunique()))
     print(Train_data[cat_fea].value_counts())
+# 将notRepairedDamage列“-”字符替换成np.nan，视为缺失值
+Train_data['notRepairedDamage'].replace('-', np.nan, inplace=True)
 
+# # 计算特征缺失值个数
+nan_count=Train_data.isnull().sum()
+# 查看存在缺失值的特征，发现notRepairedDamage，fuelType，gearbox，bodyType有缺失值，都是离散型变量，且占比较大，故填充为“未知”
+print(nan_count[nan_count > 0].sort_values(ascending=False))
+filling_columns = nan_count[nan_count > 0].index
+for column in filling_columns:
+    Train_data[column].fillna('未知', inplace=True)
+Train_data.dropna(subset=['model'], inplace=True)
 
-# 这里我包装了一个异常值处理的代码，可以随便调用。
+# 连续变量的异常值处理
 def outliers_proc(data, col_name, scale=3):
     """
     用于清洗异常值，默认用 box_plot（scale=3）进行清洗
@@ -84,58 +94,26 @@ def outliers_proc(data, col_name, scale=3):
     plt.show()
     return data_n
 
-# 我们可以删掉一些异常数据，以 power 为例。
+# 对power，kilometer，price列，删异常数据。
 Train_data = outliers_proc(Train_data, 'power', scale=3)
 Train_data = outliers_proc(Train_data, 'kilometer', scale=3)
-
 Train_data = outliers_proc(Train_data, 'price', scale=3)
 
-'''---------------------'''
-# 离散型特征的异常值处理，需要分别将每个特征取值分布查看
-# print(Train_data['notRepairedDamage'].value_counts())
-# print(Train_data['brand'].value_counts())
-# print(Train_data['bodyType'].value_counts())
-# print(Train_data['fuelType'].value_counts())
-# print(Train_data['gearbox'].value_counts())
-# print(Train_data['model'].value_counts())
-
-Train_data['notRepairedDamage'].replace('-', np.nan, inplace=True)
-''''-------'''
-# # 计算特征缺失值个数
-nan_count=Train_data.isnull().sum()
-# 查看存在缺失值的特征
-print(nan_count[nan_count > 0].sort_values(ascending=False))
-
-filling_columns = nan_count[nan_count > 0].index
-for column in filling_columns:
-    Train_data[column].fillna('未知', inplace=True)
-Train_data.dropna(subset=['model'], inplace=True)
-# print(Train_data.head())
-# Train_data.dropna(subset=nan_count[nan_count > 0].index, inplace=True)
-
-
-'''
-Train_data['train']=1
-Test_data['train']=0
-data = pd.concat([Train_data, Test_data], ignore_index=True)
-'''
 
 data=Train_data
 # 使用时间：data['creatDate'] - data['regDate']，反应汽车使用时间，一般来说价格与使用时间成反比
 # 不过要注意，数据里有时间出错的格式，所以我们需要 errors='coerce'
-
 data['used_time'] = (pd.to_datetime(data['creatDate'], format='%Y%m%d', errors='coerce') -
                             pd.to_datetime(data['regDate'], format='%Y%m%d', errors='coerce')).dt.days
+
 # 看一下空数据，有 15k 个样本的时间是有问题的，我们可以选择删除，也可以选择放着。
-# 但是这里不建议删除，因为删除缺失数据占总样本量过大，7.5%
-# 我们可以先放着，因为如果我们 XGBoost 之类的决策树，其本身就能处理缺失值，所以可以不用管；
 data['used_time'].isnull().sum()
 
 # 从邮编中提取城市信息，相当于加入了先验知识
 data['city'] = data['regionCode'].apply(lambda x : str(x)[:-2])
 data = data
 
-# 计算某品牌的销售统计量，同学们还可以计算其他特征的统计量
+# 计算某品牌的销售统计量
 # 这里要以 train 的数据计算统计量
 Train_gb = Train_data.groupby("brand")
 
@@ -154,23 +132,15 @@ for kind, kind_data in Train_gb:
 brand_fe = pd.DataFrame(all_info).T.reset_index().rename(columns={"index": "brand"})
 # data = data.merge(brand_fe, how='left', on='brand')
 brand_fe.to_excel(path+'品牌价格信息.xlsx')
-# 数据分桶 以 power 为例
-# 这时候我们的缺失值也进桶了，
-# 为什么要做数据分桶呢，原因有很多，= =
-# 1. 离散后稀疏向量内积乘法运算速度更快，计算结果也方便存储，容易扩展；
-# 2. 离散后的特征对异常值更具鲁棒性，如 age>30 为 1 否则为 0，对于年龄为 200 的也不会对模型造成很大的干扰；
-# 3. LR 属于广义线性模型，表达能力有限，经过离散化后，每个变量有单独的权重，这相当于引入了非线性，能够提升模型的表达能力，加大拟合；
-# 4. 离散后特征可以进行特征交叉，提升表达能力，由 M+N 个变量编程 M*N 个变量，进一步引入非线形，提升了表达能力；
-# 5. 特征离散后模型更稳定，如用户年龄区间，不会因为用户年龄长了一岁就变化
 
-# 当然还有很多原因，LightGBM 在改进 XGBoost 时就增加了数据分桶，增强了模型的泛化性
-
+# 对power等距离分箱
 bin = [i*20 for i in range(15)]
 data['power_bin'] = pd.cut(data['power'], bin, labels=False)
 print(data[['power_bin', 'power']].head())
 
 # bin1 = [i*4 for i in range(5)]
 # data['kilometer_bin'] = pd.cut(data['kilometer'], bin1, labels=False)
+# 对price等频率分箱
 data['price_bin'] = pd.qcut(data['price'], q=3, labels=False)
 print(data[['price_bin', 'price']].head())
 # data.dropna(subset=['price_bin'], inplace=True)
